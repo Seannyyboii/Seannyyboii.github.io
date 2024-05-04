30a107c114f5cf4c0e7c52435bc16b7244088cbe
Seannyyboii <83225739+Seannyyboii@users.noreply.github.com>
2024-05-04 17:18:21 +0800

    testing

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
-#
-#   Hello World server in Python
-#   Binds REP socket to tcp://*:5555
-#   Expects b"Hello" from client, replies with b"World"
-#
+import socket 
+import threading 
 
-import zmq
+from base64 import b64decode
+
 import numpy as np
 
 import cv2
@@ -18,60 +16,73 @@ width, height = 1280, 720
 # Hand Detector
 detector = HandDetector(maxHands=2, detectionCon=0.8)
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
+bind_ip = "127.0.0.1" 
+bind_port = 5555
 
-# Handedness
-handType, handType1 = "", ""
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip, bind_port))
+# we tell the server to start listening with
+# a maximum backlog of connections set to 5
+server.listen(5)
+
+print(f"[+] Listening on port {bind_ip} : {bind_port}")            
+
+#client handling thread
+def handle_client(client_socket):
+    while True:
+        #printing what the client sends
+        request = client_socket.recv(50000)
+        #print(f"[+] Recieved: {request}")
+
+        #sending back the packet
+        if detectHand(request, "", "") is not None:
+            client_socket.send(detectHand(request, "", "").encode())
+
+def detectHand(message, handType, handType1):
+
+    JPEG = b64decode(message)
+    img = cv2.imdecode(np.frombuffer(JPEG,dtype=np.uint8), cv2.IMREAD_COLOR)
+
+    if img is not None:
+        # Hands
+        hands, img = detector.findHands(img)
+
+        data = []
+        data1 = []
+        
+        # Landmark values - {x, y, z} * 21
+        if hands:
+            # Get the first hand detected
+            hand = hands[0]
+            # Get the landmark list
+            lmList = hand['lmList']
+            # Get the handedness
+            handType = hand["type"]
+
+            for lm in lmList:
+                data.extend([lm[0],height - lm[1],lm[2]])
+
+            # Check if a second hand is detected
+            if len(hands) == 2:
+                # Information for the second hand
+                hand1 = hands[1]
+                # Get the landmark list
+                lmList1 = hand1["lmList"]
+                # Get the handedness
+                handType1 = hand1["type"]
+
+                for lm in lmList1:
+                    data1.extend([lm[0],height - lm[1],lm[2]])
+        
+        return(handType + str(data) + "|" + handType1 + str(data1))
 
 while True:
-    #  Wait for next request from client
-    message = socket.recv()
+    # When a client connects we receive the
+    # client socket into the client variable, and
+    # the remote connection details into the addr variable
+    client, addr = server.accept()
+    print(f"[+] Accepted connection from: {addr[0]}:{addr[1]}")
 
-    # Convert ByteArray data from Unity into numpy array for OpenCV
-    image_byte_array = bytearray(message)
-    np_array = np.asarray(image_byte_array, dtype=np.uint8)
-    img = cv2.imdecode(np_array, cv2.IMREAD_UNCHANGED)
-
-    # Hands
-    hands, img = detector.findHands(img)
-
-    data = []
-    data1 = []
-    
-    # Landmark values - {x, y, z} * 21
-    if hands:
-        # Get the first hand detected
-        hand = hands[0]
-        # Get the landmark list
-        lmList = hand['lmList']
-        # Get the handedness
-        handType = hand["type"]
-
-        # print(lmList)
-        for lm in lmList:
-            data.extend([lm[0],height - lm[1],lm[2]])
-        # print(data)
-            #socket.send(str.encode(str(data)))
-        
-        # Check if a second hand is detected
-        if len(hands) == 2:
-            # Information for the second hand
-            hand1 = hands[1]
-            # Get the landmark list
-            lmList1 = hand1["lmList"]
-            # Get the handedness
-            handType1 = hand1["type"]
-
-            for lm in lmList1:
-                data1.extend([lm[0],height - lm[1],lm[2]])
-
-    #img = cv2.resize(img, (0,0), None, 0.5,0.5)
-    #cv2.imshow("Image", img)
-    # cv2.waitKey(1)
-
-    #  Send reply back to client
-    #  In the real world usage, after you finish your work, send your output here
-    # socket.send(b"World")
-    socket.send(str.encode(handType + str(data) + "|" + handType1 + str(data1)))
+    #spin up our client thread to handle the incoming data
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
